7c8ebe49b8e35dd7f8e1d2361e04e6f96b9f4640
ADHLER <adhler_25@hotmail.com>
2025-09-07 18:31:33 -0500

    actualizancdo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,1 @@
-"HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-print("HOLA MUNDO")
-print("HOLA MUNDO")
-print("HOLA MUNDO")
-# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch main
-# Your branch is up to date with 'origin/main'.
-#
-# Changes to be committed:
-#	modified:   ejercicio1.py
-print("HOLA MUNDO")
-print("HOLA MUNDO"
-      )
-print("HOLA MUNDO")
-print("HOLA MUNDO")
+
